3729be98f811fc634593778757807d0be078d23a
vishvas357 <vishvasg392@gmail.com>
2024-07-23 16:45:11 +0530

    fist day

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            print("Pressed ");
-          },
-          child: const Text("Press me"),
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Vishvas"),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage("images/flower.png"),
+          ),
         ),
       ),
     ),

5db4657f8e2c7c5a91719cfd90f93a51d388d3bf
vishvas357 <vishvasg392@gmail.com>
2024-07-23 14:35:50 +0530

    Added a button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp());
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            print("Pressed ");
+          },
+          child: const Text("Press me"),
+        ),
+      ),
+    ),
+  );
+}

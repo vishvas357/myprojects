b1d408ee839720575edda8e394a575cea2449abd
vishvas357 <vishvasg392@gmail.com>
2024-07-24 15:36:40 +0530

    quiz app (color and size)

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,71 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueAccent,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("sun rise"),
-          backgroundColor: Colors.blueGrey,
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage("images/l.jpg"),
-          ),
+  runApp(VSJApp());
+}
+
+class VSJApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Card(
+            child: Text(
+          "Quiz App",
+          style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              backgroundColor: Colors.teal),
+        )),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.red,
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Expanded(
+
+                child: Text(
+                 "delhi is the capital of india?",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.white),
+                ),
+              ),
+            ),
+
+            SizedBox(height:20),
+            Center(
+              child: Expanded(
+                child: ElevatedButton(
+                  child: Text("Submitted true"),
+                  onPressed: () {
+                    print("Submit True");
+                  },
+                ),
+              ),
+            ),
+            SizedBox(height:10),
+            Center(
+              child: Expanded(
+                child: ElevatedButton(
+                  child: Text("Submit False"),
+                  onPressed: () {
+                    print("Submitted False");
+                  },
+                ),
+              ),
+            )
+          ],
         ),
       ),
-    ),
-  );
+    ));
+  }
 }
